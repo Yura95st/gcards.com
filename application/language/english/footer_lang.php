d449b8c3ea52fecf4ef14eaa5372b263923245ab
Yura95st <yura95st@gmail.com>
2014-01-22 18:44:31 +0200

    added language folder with necessary _lang files

---
+++ application/language/english/footer_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['menu_main'] = "Main";
+$lang['menu_create_card'] = "Create card";
+$lang['menu_about'] = "About";
+$lang['menu_send_feedback'] = "Send feedback";
---
+++ application/language/russian/footer_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['menu_main'] = "Главная";
+$lang['menu_create_card'] = "Создать открытку";
+$lang['menu_about'] = "Об авторе";
+$lang['menu_send_feedback'] = "Отправить отзыв";
