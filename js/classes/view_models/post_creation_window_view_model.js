0f80b3834511ff3f2b0ce4539f47b6b358bb6c10
Yura95st <yura95st@gmail.com>
2014-02-10 03:38:59 +0200

    added post creation window viewModel

---
+++ js/classes/view_models/post_creation_window_view_model.js
@@ -0,0 +1,56 @@
+function PostCreationWindowViewModel() {
+    var self = this;
+
+    self.header = Data.values.cardPostCreatedWindow.header;
+    self.text = ko.observable("");
+    self.menuItems = ko.observableArray(Data.values.cardPostCreatedWindow.menu);
+    self.currentMenuItem = ko.observable(self.menuItems()[0]);
+    self.currentOperation = ko.observable(0);
+    self.link = ko.observable("");
+
+    self.show = function() {
+        var modalViewModel = Global.modalViewModel;
+        var html = getPostCreationWindow();
+
+        modalViewModel.content(html);
+        modalViewModel.show();
+
+        ko.applyBindings(self, $(Data.postCreationWindow.wrap)[0]);
+    };
+
+    self.showOperation = function(menuItem) {
+        var index = self.menuItems.indexOf(menuItem);
+        self.currentOperation(index);
+        self.currentMenuItem(menuItem);
+    };
+
+    var getPostCreationWindow = function() {
+        return '' +
+        '<div class="post-creation-window">' +
+            '<div class="header" data-bind="text: postCreationWindowViewModel.header"></div>' +
+            '<div class="info">' +
+                '<span class="glyphicon glyphicon-ok"></span>' +
+                '<div class="text" data-bind="text: postCreationWindowViewModel.text"></div>' +
+            '</div>' +
+            '<div class="operations-panel">' +
+                '<div class="menu" data-bind="foreach: { data: postCreationWindowViewModel.menuItems, as: \'item\' }">' +
+                    '<div class="item" data-bind="text: item.title, click: postCreationWindowViewModel.showOperation, css: {\'active\': item === postCreationWindowViewModel.currentMenuItem()}"></div>' +
+                '</div>' +
+                '<div class="operations">' +
+                    '<div class="item" data-bind="slideToggle: 0 != postCreationWindowViewModel.currentOperation()">' +
+                        '<input type="text" data-bind="value: postCreationWindowViewModel.link" />' +
+                    '</div>' +
+                '<div class="item" data-bind="slideToggle: 1 != postCreationWindowViewModel.currentOperation()">' +
+                        '<div>Share</div>' +
+                    '</div>' +
+                '<div class="item" data-bind="slideToggle: 2 != postCreationWindowViewModel.currentOperation()">' +
+                        '<div>Options</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+    };
+};
+
+
+Global.postCreationWindowViewModel = new PostCreationWindowViewModel();
