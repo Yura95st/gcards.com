34038de9179815f49bbb6c32a1e03ba412f1e4e8
Yura95st <yura95st@gmail.com>
2014-02-10 03:37:48 +0200

    added lang values

---
+++ application/language/english/data_js_lang.php
@@ -0,0 +1,38 @@
+<?php
+
+$lang['card_publish_error'] = 'Sorry, an error occurred. Please, try again later.';
+
+$lang['card_init_text'] = 'Double-click to edit';
+
+$lang['card_publish_success'] = 'Your card is successfully created! Share it.';
+
+$lang['cover_picker'] = array(
+
+    'header' => 'Cover Picker',
+    'menu' => [
+        array(
+            'id' => 1,
+            'title' => "All"
+        ),
+        array(
+            'id' => 2,
+            'title' => "New Year"
+        ),
+        array(
+            'id' => 3,
+            'title' => "St. Valentines Day"
+        ),
+        array(
+            'id' => 4,
+            'title' => "Women's Day"
+        )]
+);
+
+$lang['card_post_created_window'] = array(
+
+    'header' => 'Card is created',
+    'menu' => array(
+        'Link', 'Share', 'Options'
+    )
+);
+
